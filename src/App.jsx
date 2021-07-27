f574f805ca1e6dc0f887010fb066cba7abb15d7a
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-07-27 00:19:36 -0500

    [Update] add uuid library and add setTodo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { TodoList } from "./components/TodoList";
+import { v4 as uuid} from "uuid/dist/v4";
 
 export function App() {
   const [todo, setTodo] = useState([
     { id: 1, task: "Tarea 1", completed: false },
-    { id: 2, task: "Tarea 2", completed: true },
-    { id: 3, task: "Tarea 3", completed: false },
+    // { id: 2, task: "Tarea 2", completed: true },
+    // { id: 3, task: "Tarea 3", completed: false },
   ]);
-  const handleTodoAdd = (e) => {
-    e.preventDefault();
-    
-  }
+  const todoTaskRef = useRef();
+
+  const handleTodoAdd = () => {
+    const tasks = todoTaskRef.current.value;
+    if (tasks === "") return;
+
+    setTodo((prevTodos) => {
+      return [ ...prevTodos, { id: uuid(), tasks, completed: false }];
+    });
+  };
 
   return (
     <>
       <TodoList todos={todo} />
-      <input type="text" placeholder=" Nueva tarea" />
-      <button type="button" onClick={handleTodoAdd}> + </button>
-      <button type="button"> - </button>
+      <input ref={todoTaskRef} type="text" placeholder=" Nueva tarea" />
+      <button  onClick={handleTodoAdd}>+</button>
+      <button type="button">-</button>
     </>
   );
 }
